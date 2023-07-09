4f387aaf3c15264baf79fd5e4008d15c446d43fd
rishikeshwareddy <rishikeshwareddyj2003@gmail.com>
2023-07-09 11:17:37 +0530

    hi there

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import Translate from "./components/Translate"
+import {Route,Routes} from "react-router-dom"
 function App() {
-  return <Translate />;
+  return <Routes>
+  <Route exact path='/' element={<Translate />} />
+</Routes>;
 }
 
 export default App;
